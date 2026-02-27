8d0d8ae8d0f3ead546a77d7fe336ccf45b6446c4
Nethupa Dalugoda <nethupadalugoda2005@gmail.com>
2026-02-27 23:54:13 +0530

    addProducts Done

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,48 +1,116 @@
 import Product from "../models/product.js";
 import { isAdmin } from "./userController.js";
 
-export async function getProducts(req,res){
-    try{
-        if(isAdmin(req)){
-            const products = await Product.find()
-            res.json(products)
-        }else{
-            const products = await Product.find({isAvailable : true})
-            res.json(products)
+export async function getProducts(req, res) {
+    try {
+
+        if (isAdmin(req)) {
+            const product = await Product.find()
+            res.json(product);
+        } else {
+            const product = await Product.find({ isAvailable: true });
+            res.json(product);
         }
 
-    }catch(err){
-        res.json({
-            message : "Failed to get products",
-            error : err
+
+    } catch (err) {
+
+        res.status(500).json({
+            message: "Failed to get products",
+            error: err
+
         })
+
     }
 }
+
 
 export function saveProducts(req,res){
 
-
-    if(!isAdmin(req)){
-        res.status(403).json({
-            message : "You are not auithorized to add a product"
-        })
+    if (!isAdmin(req)) {
+        res.status(403).json(
+            {
+                message: "User not authorized to add products"
+            }
+        )
         return
     }
+    const product = new Product(req.body);
 
-    const product = new Product(
-        req.body
-    );
-
-    product.save().then(()=>{
+    product.save().then(() => {
         res.json({
-            message: "Product added successfully",
-        });
-    }).catch(() => {
+            message: "Product added successfully"
+        })
+    }).catch((err) => {
         res.json({
-            message: "Failed to add products",
-        });
-    });
+            message: "Product added failed",
+            error: err
+        })
+    })
 }
+
+// export function saveProducts(req,res){
+//     if(!isAdmin(req)){
+//         res.status(403).json({
+//             message : "You are not auithorized to add a product"
+//         })
+//         return
+//     }
+
+//     const product = new Product(req.body);
+
+//     product.save().then(()=>{
+//         res.json({
+//             message: "Product added successfully",
+//         });
+//     }).catch((err) => {
+//         console.log("ERROR SAVING PRODUCT:", err); // This should show the specific error
+//         console.log("Error name:", err.name); // Add this
+//         console.log("Error code:", err.code); // Add this
+//         console.log("Error message:", err.message); // Add this
+//         console.log(err);
+//         res.status(500).json({  // <-- ADD THIS: send 500 status code
+//             message: "Failed to add products",
+//         });
+//     });
+// }
+
+// export async function saveProducts(req,res){
+
+//     try {
+
+//         console.log("Request received")
+
+//         if(!isAdmin(req)){
+//             console.log("Not admin")
+//             return res.status(403).json({
+//                 message : "You are not authorized"
+//             })
+//         }
+
+//         console.log("Body:", req.body)
+
+//         const product = new Product(req.body);
+
+//         await product.save();
+
+//         console.log("Product saved successfully")
+
+//         return res.status(201).json({
+//             message: "Product added successfully",
+//         });
+
+//     } catch (err) {
+
+//         console.log("ERROR SAVING:", err)
+
+//         return res.status(500).json({
+//             message: "Failed to add product",
+//             error: err.message
+//         });
+//     }
+// }
+
 
 
 export async function deleteProducts(req,res){
